83a40b9d0f071eeabd87810920674b7abeea3adc
pavolh01 <95620761+pavolh01@users.noreply.github.com>
2022-03-14 16:43:14 +0100

    scss update

--- src/app/core/interfaces/color.ts
+++ src/app/core/interfaces/color.ts
@@ -4,7 +4,7 @@ export interface Color {
 }
 
 export const colors: Color[] = [
-  { label: 'FFFFFF', value: 'FFFFFF' },
+
   { label: '000000', value: '000000' },
   { label: '603813', value: '603813' },
   { label: 'FF0000', value: 'FF0000' },
@@ -12,11 +12,7 @@ export const colors: Color[] = [
 
   { label: '006837', value: 'FFD400' },
   { label: 'F15A24', value: 'F15A24' },
-  { label: 'CCCCCC', value: 'CCCCCC' },
-  { label: 'DBC0B5', value: 'DBC0B5' },
-  { label: 'FAB49B', value: 'FAB49B' },
+ 
 
-  { label: '87B2C7', value: '87B2C7' },
-  { label: 'ACD58A', value: 'ACD58A' },
-  { label: 'FFF9AE', value: 'FFF9AE' },
+  
 ];
